4e90e3ac18645c6e7802b0b580e6fe45036899e9
penyimak527 <143236724+penyimak527@users.noreply.github.com>
2023-11-29 07:43:36 +0700

    Web 1
    ini web pertamaku

---
+++ 23_Muhammad Zakiprian f/baru.js
@@ -0,0 +1,17 @@
+alert("selamat datang di webKU")
+//bio
+console.log("Namaku adalah Zaki");
+console.log("saya bersekolah di SMKN 1 Lumajang")
+console.log("umur ku adalah"+ umur);
+var umur= 16
+// digunakan untuk button
+{/* <script>
+alert("Selamat datang di tutorial belajar Javascript");
+
+function sayHello(){
+    alert("Hello!");
+}
+</script>
+</head>
+<body>
+<button onclick="sayHello()">Klik Aku!</button> */}
