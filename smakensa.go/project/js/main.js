f9a976bfe5fb1ff7a34c924410437945b6b4d244
penyimak527 <143236724+penyimak527@users.noreply.github.com>
2024-04-16 14:42:12 +0700

    Delete smakensa.go/project directory

--- smakensa.go/project/js/main.js
+++
@@ -1,6 +0,0 @@
-// Toggle class active
-const navbarnav = document.querySelector(".navbar-menu");
-// ketika hamburger menu diklik
-document.querySelector("#menu-outline").onclik = () => {
-  navbarnav.classList.toggle("active");
-};
